dad6510f391aff3196ef916c2c2b11355125377a
Jonah <jonahr7379@gmail.com>
2025-09-16 10:58:24 -0700

    Updated phone number format logic

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+function formatPhoneNumber(value) {
+  // Remove all non-digit characters
+  const digits = value.replace(/\D/g, '');
+  // Format as XXX-XXX-XXXX
+  const match = digits.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
+  if (!match) return '';
+  let formatted = '';
+  if (match[1]) formatted += match[1];
+  if (match[2]) formatted += '-' + match[2];
+  if (match[3]) formatted += '-' + match[3];
+  return formatted;
+}
+
 export const ContactForm = ({
   name,
   setName,
@@ -12,7 +25,7 @@ export const ContactForm = ({
   return (
     <form onSubmit={handleSubmit}>
       <input type='text' name='name' value={name} onChange={e => setName(e.target.value)} placeholder="Name" required></input>
-      <input type='tel' name='phone' value={phone} onChange={e => setPhone(e.target.value)} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" placeholder="Phone Number" required></input>
+      <input type='tel' name='phone' value={phone} onChange={e => setPhone(formatPhoneNumber(e.target.value))} placeholder="Phone Number" required></input>
       <input type='email' name='email' value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required></input>
       <input type='submit' value='Add Contact'/>
     </form>
